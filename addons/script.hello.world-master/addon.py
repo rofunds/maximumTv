73e425855e17e87d7b04b0775820e2a42918e923
rofunds <rofunds@gmail.com>
2016-05-21 16:39:55 -0400

    Update addon.py

--- addons/script.hello.world-master/addon.py
+++ addons/script.hello.world-master/addon.py
@@ -8,16 +8,8 @@ addonname   = addon.getAddonInfo('name')
 
 
 def update():
-    subprocess.Popen("git pull", cwd = "C:\\Program Files (x86)\\Kodi\\")
-    #subprocess.cwd('C:\\Program Files (x86)\\Kodi\\addons')k
-    #subprocess.call(["git pull"])
-    #os.chdir("home//osmc//.kodi//")
-    #os.chdir("C:\\Program Files (x86)\\Kodi\\addons")
-    #os.system("cd", "git pull")
+    subprocess.Popen("git pull", cwd = "home//osmc//.kodi//")
 
-
-
-line1 = "Do you want to update your box at this time?"
 
 
 dialog = xbmcgui.Dialog()
@@ -27,4 +19,5 @@ if i == 0:
     pass
 
 else:
+    dialog.ok("Max TV Wizard", "Please restart box after update.")
     update()
